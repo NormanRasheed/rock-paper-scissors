fafd6fd73ead6b7c4ccf533ad9721442732c6d9d
Rasheed Norman <rasheednorman5@gmail.com>
2023-08-22 20:17:43 -0400

    added most of the logic for the rock paper scissors game. Next have to figure out how to go about handling the scoring portion of the project.

--- script.js
+++ script.js
@@ -4,3 +4,45 @@ function getComputerChoice(){
     let index = Math.floor(Math.random() * choices.length);
     return choices[index];
 }
+function playRound(playerSelection = prompt("make your choice"), computerSelection = getComputerChoice()) {
+
+    playerSelection = playerSelection.toLowerCase();
+
+    if(playerSelection === "rock" && computerSelection === "paper"){
+        return "You lose, paper beats rock!"
+    };
+    if(playerSelection === "paper" && computerSelection === "rock"){
+        return "You win, paper beats rock!"
+    };
+    if(playerSelection === "scissors" && computerSelection === "paper"){
+        return "You win, scissors beats paper!"
+    };
+    if(playerSelection === "paper" && computerSelection === "scissors"){
+        return "You lose, scissors beats paper!"
+    };
+    if(playerSelection === "scissors" && computerSelection === "rock"){
+        return "You lose, rock beats scissors!"
+    };
+    if(playerSelection === "rock" && computerSelection === "scissors"){
+        return "You win, rock beats scissors!"
+    };
+    if(playerSelection === "scissors" && computerSelection === "scissors"){
+        return "Draw"
+    };
+    if(playerSelection === "rock" && computerSelection === "rock"){
+        return "Draw"
+    };
+    if(playerSelection === "paper" && computerSelection === "paper"){
+        return "Draw"
+    };
+  }
+
+  function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++){
+        console.log(playRound(playerSelection = prompt("make your choice"), computerSelection = getComputerChoice()));
+    }
+    return
+  }
