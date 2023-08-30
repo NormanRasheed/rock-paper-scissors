bca9c4220a9bd5d22a99e55f48ff65d0c28b9ce2
Rasheed Norman <rasheednorman5@gmail.com>
2023-08-30 19:53:54 -0400

    Added the buttons and the DOM on click functionality

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let choices = ["rock", "paper", "scissors"];
-
-function getComputerChoice(){
+function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
     let index = Math.floor(Math.random() * choices.length);
     return choices[index];
 }
@@ -8,58 +7,63 @@ function playRound(playerSelection = prompt("make your choice"), computerSelecti
 
     playerSelection = playerSelection.toLowerCase();
 
-    if(playerSelection === "rock" && computerSelection === "paper"){
+    if (playerSelection === "rock" && computerSelection === "paper") {
         return "You lose, paper beats rock!"
     };
-    if(playerSelection === "paper" && computerSelection === "rock"){
+    if (playerSelection === "paper" && computerSelection === "rock") {
         return "You win, paper beats rock!"
     };
-    if(playerSelection === "scissors" && computerSelection === "paper"){
+    if (playerSelection === "scissors" && computerSelection === "paper") {
         return "You win, scissors beats paper!"
     };
-    if(playerSelection === "paper" && computerSelection === "scissors"){
+    if (playerSelection === "paper" && computerSelection === "scissors") {
         return "You lose, scissors beats paper!"
     };
-    if(playerSelection === "scissors" && computerSelection === "rock"){
+    if (playerSelection === "scissors" && computerSelection === "rock") {
         return "You lose, rock beats scissors!"
     };
-    if(playerSelection === "rock" && computerSelection === "scissors"){
+    if (playerSelection === "rock" && computerSelection === "scissors") {
         return "You win, rock beats scissors!"
     };
-    if(playerSelection === "scissors" && computerSelection === "scissors"){
+    if (playerSelection === "scissors" && computerSelection === "scissors") {
         return "Draw"
     };
-    if(playerSelection === "rock" && computerSelection === "rock"){
+    if (playerSelection === "rock" && computerSelection === "rock") {
         return "Draw"
     };
-    if(playerSelection === "paper" && computerSelection === "paper"){
+    if (playerSelection === "paper" && computerSelection === "paper") {
         return "Draw"
     };
-  }
+}
 
-  function game(){
-    let playerScore = 0;
-    let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-    for(let i = 0; i < 5; i++){
-       let result = playRound(playerSelection = prompt("make your choice"), computerSelection = getComputerChoice());
+function game() { // for(let i = 0; i < 5; i++){
+    let result = playRound(playerSelection = prompt("make your choice"), computerSelection = getComputerChoice());
 
-       console.log(result);
+    console.log(result);
 
-       if(result.includes("win")){
+    if (result.includes("win")) {
         playerScore++;
-       };
-       if(result.includes("lose")){
+    };
+    if (result.includes("lose")) {
         computerScore++;
-       };
-       console.log(`The player score is:${playerScore}`);
-       console.log(`The computer score is:${computerScore}`);
-    }
-    if(playerScore > computerScore){
+    };
+    console.log(`The player score is:${playerScore}`);
+    console.log(`The computer score is:${computerScore}`);
+    // }
+    if (playerScore === 5) {
         return "Player has won"
-    } else if(playerScore === computerScore){
+    } else if (playerScore === computerScore) {
         return "The game is a draw"
     } else {
         return "The computer has won"
     }
-  }
+}
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(playerSelection = button.id, computerSelection = getComputerChoice());
+    });
+});
